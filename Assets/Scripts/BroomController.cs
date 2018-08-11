06b3b3ab30b8fd7c7f599bbeb52a6894c0d8df1c
Blazingbeta <Btcleveland@gmail.com>
2018-08-11 10:50:02 -0600

    Initial Setup, some basic movement setup

---
+++ Assets/Scripts/BroomController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomController : MonoBehaviour
+{
+	[SerializeField] Vector3 m_basePlayerOffset = Vector3.zero;
+	Camera m_mainCam = null;
+	bool m_isInAnimation = false;
+	private void Start()
+	{
+		m_mainCam = Camera.main;
+	}
+	private void Update()
+	{
+		//Only update position if the player isn't in the middle of an attack
+		if (!m_isInAnimation)
+		{
+			//Get the direction towards the mouse
+			Vector3 mousePos = m_mainCam.ScreenToWorldPoint(Input.mousePosition);
+			mousePos.z = 0;
+			Vector3 direction = (transform.parent.position - mousePos).normalized;
+			//rotate the offset * direction to make it hover in front of the player
+			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+			Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
+			transform.localPosition = rot * m_basePlayerOffset;
+			transform.rotation = rot;
+		}
+
+		//attaack if input is given
+		if (Input.GetMouseButtonDown(0))
+		{
+			//StartCoroutine();
+		}
+	}
+	private void LateUpdate()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField] float m_movementForce = 5.0f;
+	[SerializeField] float m_movementDragCoeffecient = 0.8f;
+	[SerializeField] float m_movementMass = 3.0f;
+	[SerializeField] float m_movementVelocitySleepSquared = 1.0f;
+	Vector3 m_currentVelocity = Vector3.zero;
+	private void FixedUpdate()
+	{
+		//Get the user's input and add it to the velocity
+		Vector3 inputDir = Vector3.zero;
+		inputDir.x = Input.GetAxis("Horizontal");
+		inputDir.y = Input.GetAxis("Vertical");
+		if (inputDir.sqrMagnitude > 1)
+		{
+			inputDir = inputDir.normalized;
+		}
+		//Input direction * force amount / mass
+		m_currentVelocity += (inputDir * m_movementForce / m_movementMass) / 60.0f;
+		//Apply the drag
+		//force = v^2 * drag in negative v normalized dir
+		Vector3 dragForce = -m_currentVelocity.normalized;
+		dragForce *= m_currentVelocity.sqrMagnitude * m_movementDragCoeffecient;
+		dragForce /= m_movementMass;
+		m_currentVelocity += dragForce;
+		//If there is no input and the velocity is below the cutoff
+		if (inputDir.sqrMagnitude == 0 && m_currentVelocity.sqrMagnitude <= m_movementVelocitySleepSquared)
+		{
+			m_currentVelocity = Vector3.zero;
+		}
+		//Apply the velocity
+		transform.position += m_currentVelocity;
+	}
+}
